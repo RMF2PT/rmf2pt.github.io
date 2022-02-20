99f3e8dfb77912619ebf16d55f040081622f9bfa
rmf2pt <rmf2pt@gmail.com>
2022-02-20 10:51:54 +0000

    Add toggle active player TicTacToe

--- projects/JS TicTacToe/script.js
+++ projects/JS TicTacToe/script.js
@@ -1,5 +1,7 @@
 let squares = [];
-let symbol = 'X';
+let symbol = '';
+let playerX = document.getElementById('playerX');
+let playerO = document.getElementById('playerO');
 
 function resetGame() {
   for (var i = 0; i < 9; i++) {
@@ -7,6 +9,21 @@ function resetGame() {
     squares[i].addEventListener('click', myChoice);
     squares[i].innerHTML = '';
     squares[i].removeAttribute('disabled');
+    symbol = 'X';
+    playerX.classList.remove('d-none');
+    playerO.classList.add('d-none');
+  }
+}
+
+function changePlayer() {
+  if (symbol == 'O') {
+    symbol = 'X';
+    playerX.classList.remove('d-none');
+    playerO.classList.add('d-none');
+  } else {
+    symbol = 'O';
+    playerO.classList.remove('d-none');
+    playerX.classList.add('d-none');
   }
 }
 
@@ -17,7 +34,7 @@ function myChoice() {
     alert(`Player ${symbol} is the winner!`);
     resetGame();
   } else {
-    symbol = symbol == 'X' ? 'O' : 'X'; // change symbol for next player
+    changePlayer();
   }
 }
 
