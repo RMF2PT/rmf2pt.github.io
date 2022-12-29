1db9821fb259e6e5374e7ea91082b117391891ef
rmf2pt <rmf2pt@gmail.com>
2022-12-29 09:54:48 +0000

    Fix quote generator with new API

--- projects/QuoteGenerator/quoteGenerator.js
+++ projects/QuoteGenerator/quoteGenerator.js
@@ -1,21 +1,21 @@
-var url = 'https://programming-quotes-api.herokuapp.com/Quotes';
+var url = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
-var nextImg = function() {
-  return ("url(https://picsum.photos/g/800/600?image="+Math.floor(Math.random() * 100) + 1);
+var nextImg = function () {
+  return ("url(https://picsum.photos/g/800/600?image=" + Math.floor(Math.random() * 100) + 1);
 };
 
-var changeBackgroundImage = function() {
-  $('.wrapper').fadeOut(400, function() {
+var changeBackgroundImage = function () {
+  $('.wrapper').fadeOut(400, function () {
     $(this).css('background-image', nextImg());
     $(this).fadeIn('fast');
   });
 }
 
-var getNewQuote = function(data) {
+var getNewQuote = function (data) {
   changeBackgroundImage();
   var randomQuote = data[Math.floor(Math.random() * 500) + 1]
-  $('blockquote').fadeOut('slow', function() {
-    $(".quote").text(randomQuote.en);
+  $('blockquote').fadeOut('slow', function () {
+    $(".quote").text(randomQuote.text);
     if (randomQuote.author === undefined || randomQuote.author === "") {
       $('.author').text('Anonymous');
     } else {
@@ -26,17 +26,17 @@ var getNewQuote = function(data) {
   })
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   // new quote when the page loads
   $.getJSON(url, getNewQuote);
-  
+
   // new quote on click
-  $('#getNewQuote').on('click', function() {
+  $('#getNewQuote').on('click', function () {
     $.getJSON(url, getNewQuote);
   });
-  
+
   // tweet quote
-  $('#tweet').click(function() {
+  $('#tweet').click(function () {
     var quoteText = $('.quote').text().trim();
     var author = $('.author').text().trim();
     var tweetText = quoteText + " " + author;
